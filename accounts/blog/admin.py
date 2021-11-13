91edab5cda6d977c5160999ed0db495273504bb4
sreedevisatheesh <90721804+sreedevisatheesh@users.noreply.github.com>
2021-11-13 11:26:32 +0530

    Delete accounts/blog directory

--- accounts/blog/admin.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.contrib import admin
-
-from blog.models import Category, Comment, Newsletter, Post
-
-admin.site.register(Post)
-admin.site.register(Category)
-admin.site.register(Comment)
-admin.site.register(Newsletter)
